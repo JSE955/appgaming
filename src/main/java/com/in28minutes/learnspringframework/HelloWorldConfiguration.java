37008defa45f7887768ac51ff12236ac65e925bc
JEdwards <jordanscottedwards@gmail.com>
2023-02-03 15:30:39 -0500

    Implemented functionality to manage matching beans

--- src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -1,7 +1,9 @@
 package com.in28minutes.learnspringframework;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 // Eliminate verbosity in creating Java Beans
 // Public accessor methods, constructor,
@@ -38,12 +40,25 @@ public class HelloWorldConfiguration {
 		return new Person(name, age, address3); // name, age, address2
 	}
 	
+	@Bean
+	@Primary
+	public Person person4Parameters(String name, int age, Address address) {
+		return new Person(name, age, address); // name, age, address2
+	}
+	
+	@Bean
+	public Person person5Qualifier(String name, int age, @Qualifier("address3qualifier") Address address) {
+		return new Person(name, age, address); // name, age, address2
+	}
+	
 	@Bean("address2")
+	@Primary
 	public Address address() {
 		return new Address("123 Street", "Charleston");
 	}
 	
 	@Bean("address3")
+	@Qualifier("address3qualifier")
 	public Address address3() {
 		return new Address("12390 Avenue", "Minneapolis");
 	}
