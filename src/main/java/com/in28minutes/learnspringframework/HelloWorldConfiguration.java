ad6e07768f13de36b9b5983f957d7626bf6e0f06
JEdwards <jordanscottedwards@gmail.com>
2023-02-03 15:04:13 -0500

    Added new bean configurations

--- src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 // Public accessor methods, constructor,
 // equals, hashcode and toString are automatically created.
 // Released in JDK 16.
-record Person (String name, int age) {};
+record Person (String name, int age, Address address) {};
 record Address (String firstLine, String city) {};
 
 @Configuration
@@ -25,11 +25,26 @@ public class HelloWorldConfiguration {
 	
 	@Bean
 	public Person person() {
-		return new Person("Izzy", 33);
+		return new Person("Izzy", 33, new Address("567 Street", "Hanahan"));
 	}
 	
 	@Bean
+	public Person person2MethodCall() {
+		return new Person(name(), age(), address()); // name, age
+	}
+	
+	@Bean
+	public Person person3Parameters(String name, int age, Address address3) {
+		return new Person(name, age, address3); // name, age, address2
+	}
+	
+	@Bean("address2")
 	public Address address() {
 		return new Address("123 Street", "Charleston");
+	}
+	
+	@Bean("address3")
+	public Address address3() {
+		return new Address("12390 Avenue", "Minneapolis");
 	}
 }
